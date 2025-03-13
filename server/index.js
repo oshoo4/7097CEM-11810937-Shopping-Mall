fbcd2307b3d86a253bfaa94553f13a5353ec7502
Olajide Osho <oshoo4@coventry.ac.uk>
2025-03-13 22:47:46 +0000

    Initial project setup with React, Express, MongoDB and Heroku CI/CD

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,6 +18,14 @@ mongoose.connect(process.env.MONGODB_URI)
 app.get('/api/test', (req, res) => {
     res.json({ message: 'Server is working!' });
 });
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
